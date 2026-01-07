65debf29dcd0168c1f9b73ddde8b0233e9bcff28
mounika391 <mounikayeadkey@gmail.com>
2026-01-07 12:18:14 +0530

    Add files via upload

---
+++ lab1/pattern3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("enter the rows;");
+scanf("%d",&n);
+for(int i=0;i<=n;i++)
+{
+for(int j=1;j<=n-i;j++)
+{
+printf(" ");
+}
+for(int k=1;k<=i-1;k++)
+{
+printf("* ");
+
+}
+
+printf("\n");
+}
+
+}
+
